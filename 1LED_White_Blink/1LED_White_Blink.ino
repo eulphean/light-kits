647d48de634d21f77b1d557e70aada0b17eacc4d
Amay Kataria <amkatari@microsoft.com>
2018-06-27 21:37:28 -0500

    Update example 1 description.

--- 1LED_White_Blink/1LED_White_Blink.ino
+++ 1LED_White_Blink/1LED_White_Blink.ino
@@ -16,14 +16,16 @@
  */
 
 /*  * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   
+
   Author: Amay Kataria
   Date: 05/06/2018
-  
-  
+  Description: This example lets you blink an LED at a specific 
+  rate. The color of the LED is white. You can change the color
+  of the LED by changing the RGB value, which is set. 
+    
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-// Define LED pins. 
+// Define LED driver pins.
 const int led1[] = {3, 5, 6};
 
 // Record the last time to track the time of the LED. 
@@ -32,9 +34,9 @@ unsigned long lastTime;
 // Current LED state. 
 bool ledOn = true; 
 
-int blinkTime = 1000; // 1 second - 1000 ms
 // By decreasing this number, you can make the led blink faster. 
 // This is the time it's ON and then OFF. 
+int blinkTime = 100; // 1 second - 1000 ms
 
 void setup() {
   // Initialize LED1
@@ -52,9 +54,9 @@ void loop() {
   // LED should be white and blinking every *blinkTime seconds*.  
   if (currentTime - lastTime < blinkTime) {
     if (ledOn) {
-      setRGB1Color(255, 255, 255);
+      setRGBColor(255, 255, 255);
     } else {
-      setRGB1Color(0, 0, 0);
+      setRGBColor(0, 0, 0);
     }
   } else {
     lastTime = millis(); // Reset time. 
@@ -63,10 +65,9 @@ void loop() {
 }
 
 // LED1 color setter
-void setRGB1Color(int red, int green, int blue) {
+void setRGBColor(int red, int green, int blue) {
    analogWrite(led1[0], red);
    analogWrite(led1[1], green);
    analogWrite(led1[2], blue);
 }
-
 
